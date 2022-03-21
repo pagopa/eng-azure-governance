5ded94f3d934a58dea2e184eee0b7bea2c72cbcd
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-03-21 10:14:03 +0100

    files reorder

--- src/policy/02_policy_set_3_prod.tf
+++ src/policy/02_policy_set_3_prod.tf
@@ -1,6 +1,4 @@
-#
 # Enforced
-#
 resource "azurerm_policy_set_definition" "prod_set_enforced" {
   name                  = "pagopa_prod_set_enforced"
   policy_type           = "Custom"
@@ -41,9 +39,7 @@ PARAMETERS
   }
 }
 
-#
 # Advices
-#
 resource "azurerm_policy_set_definition" "prod_set_advice" {
   name                  = "pagopa_prod_set_advice"
   policy_type           = "Custom"
@@ -81,5 +77,72 @@ PARAMETERS
       }
     }
     VALUE
+  }
+}
+
+#
+# Asingment
+#
+
+locals {
+  list_allowed_locations_prod = jsonencode(var.prod_allowed_locations)
+  list_allow_skus_raw_prod    = jsonencode(var.prod_vm_skus_allowed)
+}
+
+resource "azurerm_management_group_policy_assignment" "prod_set_enforced_2_root_sl_pay" {
+  name                 = "pa_prodsetenf2rootslpay"
+  display_name         = "PagoPA/PROD set enforce 2 Mgmt root sl servizi e pagamenti"
+  policy_definition_id = azurerm_policy_set_definition.prod_set_enforced.id
+  management_group_id  = data.azurerm_management_group.prod_sl_pagamenti_servizi.id
+
+  location = var.location
+  enforce  = true
+
+  metadata = <<METADATA
+    {
+        "category": "${var.metadata_category_name}",
+        "version": "v1.0.0"
+    }
+METADATA
+
+  parameters = <<PARAMS
+  {
+      "listOfAllowedLocations": {
+          "value": ${local.list_allowed_locations_prod}
+      }
+  }
+PARAMS
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_management_group_policy_assignment" "prod_set_advice_2_root_sl_pay" {
+  name                 = "pa_prodsetadv2rootslpay"
+  display_name         = "PagoPA/PROD set advice 2 Mgmt root sl servizi e pagamenti"
+  policy_definition_id = azurerm_policy_set_definition.prod_set_advice.id
+  management_group_id  = data.azurerm_management_group.prod_sl_pagamenti_servizi.id
+
+  location = var.location
+  enforce  = false
+
+  metadata = <<METADATA
+    {
+        "category": "${var.metadata_category_name}",
+        "version": "v1.0.0"
+    }
+METADATA
+
+  parameters = <<PARAMS
+  {
+      "listOfAllowedSKUs": {
+          "value": ${local.list_allow_skus_raw_prod}
+      }
+  }
+PARAMS
+
+  identity {
+    type = "SystemAssigned"
   }
 }
