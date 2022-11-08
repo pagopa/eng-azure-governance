8814a38da44080c02713ed078aad3ffe30d72eb0
pasqualedevita <>
2022-11-08 09:56:06 +0100

    feat: Add Audit logs for Postgresql Single Server

---
+++ src/02_policy_audit_logs/01_postgresql_single_server.tf
@@ -0,0 +1,100 @@
+resource "azurerm_policy_definition" "audit_logs_postgresql_single_server_log_analytics" {
+  name                = "audit_logs_postgresql_single_server_log_analytics"
+  policy_type         = "Custom"
+  mode                = "Indexed"
+  display_name        = "PagoPA Add Diagnostic Settings for audit logs for Postgresql Single Server to Log Analytics workspace"
+  management_group_id = data.azurerm_management_group.pagopa.id
+
+  metadata = <<METADATA
+    {
+        "category": "${var.metadata_category_name}",
+        "version": "v1.0.0",
+        "securityCenter": {
+		      "RemediationDescription": "Add Diagnostic Settings for audit logs for Postgresql Single Server to Log Analytics workspace",
+		      "Severity": "High"
+        }
+    }
+METADATA
+
+  parameters = <<PARAMETERS
+  {
+    "diagnosticsSettingName": {
+      "type": "String",
+      "metadata": {
+        "displayName": "Setting name",
+        "description": "Name of the diagnostic settings."
+      },
+      "defaultValue": "AuditLogs_LogAnalytics"
+    },
+    "logAnalytics": {
+      "type": "String",
+      "metadata": {
+        "displayName": "Log Analytics workspace",
+        "description": "Specify the Log Analytics workspace the Key Vault should be connected to.",
+        "strongType": "omsWorkspace",
+        "assignPermissions": true
+      }
+    }
+  }
+PARAMETERS
+
+  policy_rule = templatefile("./policy_rules/postgresql_single_server_log_analytics.json", {
+    roleDefinitionIds_audit_logs_contributor    = data.azurerm_role_definition.audit_logs_contributor.id,
+    roleDefinitionIds_log_analytics_contributor = data.azurerm_role_definition.log_analytics_contributor.id,
+  })
+
+}
+
+resource "azurerm_policy_definition" "audit_logs_postgresql_single_server_storage_account" {
+  name                = "audit_logs_postgresql_single_server_storage_account"
+  policy_type         = "Custom"
+  mode                = "Indexed"
+  display_name        = "PagoPA Add Diagnostic Settings for audit logs for Postgresql Single Server to Storage Account"
+  management_group_id = data.azurerm_management_group.pagopa.id
+
+  metadata = <<METADATA
+    {
+        "category": "${var.metadata_category_name}",
+        "version": "v1.0.0",
+        "securityCenter": {
+		      "RemediationDescription": "Add Diagnostic Settings for audit logs for Postgresql Single Server to Storage Account",
+		      "Severity": "High"
+        }
+    }
+METADATA
+
+  parameters = <<PARAMETERS
+  {
+    "diagnosticsSettingName": {
+      "type": "String",
+      "metadata": {
+        "displayName": "Setting name",
+        "description": "Name of the diagnostic settings."
+      },
+      "defaultValue": "AuditLogs_StorageAccount"
+    },
+    "storageAccount": {
+      "type": "String",
+      "metadata": {
+        "displayName": "Storage Account",
+        "description": "Specify the Storage Account the Key Vault should be connected to.",
+        "assignPermissions": true
+      }
+    },
+    "location": {
+      "type": "String",
+      "metadata": {
+        "displayName": "Resource location",
+        "description": "Specify the resource location.",
+        "assignPermissions": true
+      }
+    }
+  }
+PARAMETERS
+
+  policy_rule = templatefile("./policy_rules/postgresql_single_server_storage_account.json", {
+    roleDefinitionIds_audit_logs_contributor    = data.azurerm_role_definition.audit_logs_contributor.id,
+    roleDefinitionIds_log_analytics_contributor = data.azurerm_role_definition.log_analytics_contributor.id,
+  })
+
+}
--- src/03_policy_set/01_audit_logs.tf
+++ src/03_policy_set/01_audit_logs.tf
@@ -18,25 +18,28 @@ variable "audit_logs_storage_id_northeurope" {
 
 locals {
   audit_logs = {
-    metadata_category_name                                 = "pagopa_env_prod"
-    keyvault_workspaceid_reference_id                      = "keyvault_workspaceid"
-    keyvault_storageid_westeurope_reference_id             = "keyvault_storageid_westeurope"
-    keyvault_storageid_northeurope_reference_id            = "keyvault_storageid_northeurope"
-    application_gateway_workspaceid_reference_id           = "application_gateway_workspaceid"
-    application_gateway_storageid_westeurope_reference_id  = "application_gateway_storageid_westeurope"
-    application_gateway_storageid_northeurope_reference_id = "application_gateway_storageid_northeurope"
-    container_registry_workspaceid_reference_id            = "container_registry_workspaceid"
-    container_registry_storageid_westeurope_reference_id   = "container_registry_storageid_westeurope"
-    container_registry_storageid_northeurope_reference_id  = "container_registry_storageid_northeurope"
-    kubernetes_cluster_workspaceid_reference_id            = "kubernetes_cluster_workspaceid"
-    kubernetes_cluster_storageid_westeurope_reference_id   = "kubernetes_cluster_storageid_westeurope"
-    kubernetes_cluster_storageid_northeurope_reference_id  = "kubernetes_cluster_storageid_northeurope"
-    api_management_workspaceid_reference_id                = "api_management_workspaceid"
-    api_management_storageid_westeurope_reference_id       = "api_management_storageid_westeurope"
-    api_management_storageid_northeurope_reference_id      = "api_management_storageid_northeurope"
-    postgresql_flexible_workspaceid_reference_id           = "postgresql_flexible_workspaceid"
-    postgresql_flexible_storageid_westeurope_reference_id  = "postgresql_flexible_storageid_westeurope"
-    postgresql_flexible_storageid_northeurope_reference_id = "postgresql_flexible_storageid_northeurope"
+    metadata_category_name                                      = "pagopa_env_prod"
+    keyvault_workspaceid_reference_id                           = "keyvault_workspaceid"
+    keyvault_storageid_westeurope_reference_id                  = "keyvault_storageid_westeurope"
+    keyvault_storageid_northeurope_reference_id                 = "keyvault_storageid_northeurope"
+    application_gateway_workspaceid_reference_id                = "application_gateway_workspaceid"
+    application_gateway_storageid_westeurope_reference_id       = "application_gateway_storageid_westeurope"
+    application_gateway_storageid_northeurope_reference_id      = "application_gateway_storageid_northeurope"
+    container_registry_workspaceid_reference_id                 = "container_registry_workspaceid"
+    container_registry_storageid_westeurope_reference_id        = "container_registry_storageid_westeurope"
+    container_registry_storageid_northeurope_reference_id       = "container_registry_storageid_northeurope"
+    kubernetes_cluster_workspaceid_reference_id                 = "kubernetes_cluster_workspaceid"
+    kubernetes_cluster_storageid_westeurope_reference_id        = "kubernetes_cluster_storageid_westeurope"
+    kubernetes_cluster_storageid_northeurope_reference_id       = "kubernetes_cluster_storageid_northeurope"
+    api_management_workspaceid_reference_id                     = "api_management_workspaceid"
+    api_management_storageid_westeurope_reference_id            = "api_management_storageid_westeurope"
+    api_management_storageid_northeurope_reference_id           = "api_management_storageid_northeurope"
+    postgresql_flexible_workspaceid_reference_id                = "postgresql_flexible_workspaceid"
+    postgresql_flexible_storageid_westeurope_reference_id       = "postgresql_flexible_storageid_westeurope"
+    postgresql_flexible_storageid_northeurope_reference_id      = "postgresql_flexible_storageid_northeurope"
+    postgresql_single_server_workspaceid_reference_id           = "postgresql_single_server_workspaceid"
+    postgresql_single_server_storageid_westeurope_reference_id  = "postgresql_single_server_storageid_westeurope"
+    postgresql_single_server_storageid_northeurope_reference_id = "postgresql_single_server_storageid_northeurope"
   }
 }
 
@@ -69,7 +72,10 @@ resource "azurerm_policy_set_definition" "audit_logs" {
           "${local.audit_logs.api_management_storageid_northeurope_reference_id} : ${local.audit_logs.api_management_storageid_northeurope_reference_id}": "${data.azurerm_management_group.pagopa.id}",
           "${local.audit_logs.postgresql_flexible_workspaceid_reference_id} : ${local.audit_logs.postgresql_flexible_workspaceid_reference_id}": "${data.azurerm_management_group.pagopa.id}",
           "${local.audit_logs.postgresql_flexible_storageid_westeurope_reference_id} : ${local.audit_logs.postgresql_flexible_storageid_westeurope_reference_id}": "${data.azurerm_management_group.pagopa.id}",
-          "${local.audit_logs.postgresql_flexible_storageid_northeurope_reference_id} : ${local.audit_logs.postgresql_flexible_storageid_northeurope_reference_id}": "${data.azurerm_management_group.pagopa.id}"
+          "${local.audit_logs.postgresql_flexible_storageid_northeurope_reference_id} : ${local.audit_logs.postgresql_flexible_storageid_northeurope_reference_id}": "${data.azurerm_management_group.pagopa.id}",
+          "${local.audit_logs.postgresql_single_server_workspaceid_reference_id} : ${local.audit_logs.postgresql_single_server_workspaceid_reference_id}": "${data.azurerm_management_group.pagopa.id}",
+          "${local.audit_logs.postgresql_single_server_storageid_westeurope_reference_id} : ${local.audit_logs.postgresql_single_server_storageid_westeurope_reference_id}": "${data.azurerm_management_group.pagopa.id}",
+          "${local.audit_logs.postgresql_single_server_storageid_northeurope_reference_id} : ${local.audit_logs.postgresql_single_server_storageid_northeurope_reference_id}": "${data.azurerm_management_group.pagopa.id}"
         }
     }
 METADATA
@@ -326,6 +332,50 @@ METADATA
   policy_definition_reference {
     policy_definition_id = data.terraform_remote_state.policy_audit_logs.outputs.audit_logs_postgresql_flexible_storage_account_id
     reference_id         = local.audit_logs.postgresql_flexible_storageid_northeurope_reference_id
+    parameter_values     = <<VALUE
+    {
+      "storageAccount": {
+        "value": "${var.audit_logs_storage_id_northeurope}"
+      },
+      "location": {
+        "value": "northeurope"
+      }
+    }
+    VALUE
+  }
+
+  ## Postgresql Flexible
+
+  policy_definition_reference {
+    policy_definition_id = data.terraform_remote_state.policy_audit_logs.outputs.audit_logs_postgresql_single_server_log_analytics_id
+    reference_id         = local.audit_logs.postgresql_single_server_workspaceid_reference_id
+    parameter_values     = <<VALUE
+    {
+      "logAnalytics": {
+        "value": "${var.audit_logs_workspace_id}"
+      }
+    }
+    VALUE
+  }
+
+  policy_definition_reference {
+    policy_definition_id = data.terraform_remote_state.policy_audit_logs.outputs.audit_logs_postgresql_single_server_storage_account_id
+    reference_id         = local.audit_logs.postgresql_single_server_storageid_westeurope_reference_id
+    parameter_values     = <<VALUE
+    {
+      "storageAccount": {
+        "value": "${var.audit_logs_storage_id_westeurope}"
+      },
+      "location": {
+        "value": "westeurope"
+      }
+    }
+    VALUE
+  }
+
+  policy_definition_reference {
+    policy_definition_id = data.terraform_remote_state.policy_audit_logs.outputs.audit_logs_postgresql_single_server_storage_account_id
+    reference_id         = local.audit_logs.postgresql_single_server_storageid_northeurope_reference_id
     parameter_values     = <<VALUE
     {
       "storageAccount": {
