bd95dd47733a0a91b28d545dd0865aff724e8656
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-11-15 17:45:36 +0100

    updated versions terraform providers

--- src/governance/00_main.tf
+++ src/governance/00_main.tf
@@ -1,22 +1,18 @@
 terraform {
-  required_version = ">=1.1.1"
+  required_version = ">= 1.3.0"
 
   backend "azurerm" {
     resource_group_name = "io-infra-rg"
-    container_name      = "azureadstate"
-    key                 = "terraform.tfstate"
+    container_name      = "azurermstate"
+    key                 = "azure-governance.tfstate"
   }
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.99.0"
+      version = ">= 3.31.0"
     }
 
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "=2.10.0"
-    }
     time = {
       source  = "hashicorp/time"
       version = "~> 0.7.0"
