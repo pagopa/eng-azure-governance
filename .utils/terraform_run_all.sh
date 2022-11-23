a988850fe730851a7356206dbfc771f73575585c
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-11-23 12:38:04 +0100

    terraform-run-all updated with project

--- .utils/terraform_run_all.sh
+++ .utils/terraform_run_all.sh
@@ -14,14 +14,13 @@ set -eu
 pids=()
 ACTION="$1"
 
-array=(
+policy_folders=(
   'src/01_custom_roles'
   'src/02_policy_audit_logs'
   'src/02_policy_resource_lock'
   'src/02_policy_tags_inherit_from_subscription'
   'src/03_policy_set'
   'src/04_policy_assignments'
-  'src/governance'
 )
 
 function rm_terraform {
@@ -31,17 +30,42 @@ function rm_terraform {
 echo "[INFO] 🪚  Delete all .terraform folders"
 rm_terraform
 
-echo "[INFO] 🏁 Init all terraform repos"
-for folder in "${array[@]}" ; do
+echo "[INFO] 🏁 Init all policy terraform repos"
+az account set -s common
+
+for folder in "${policy_folders[@]}" ; do
     echo "${folder}"
     pushd "$(pwd)/${folder}"
         echo "$folder"
         echo "🔬 folder: $(pwd) in under terraform: $ACTION action"
-        sh terraform "$ACTION" &
+        terraform "$ACTION" &
 
         pids+=($!)
     popd
 done
+
+# Wait for each specific process to terminate.
+# Instead of this loop, a single call to 'wait' would wait for all the jobs
+# to terminate, but it would not give us their exit status.
+#
+for pid in "${pids[@]}"; do
+  #
+  # Waiting on a specific PID makes the wait command return with the exit
+  # status of that process. Because of the 'set -e' setting, any exit status
+  # other than zero causes the current shell to terminate with that exit
+  # status as well.
+  #
+  wait "$pid"
+done
+
+echo "[INFO] Init governance"
+pushd "$(pwd)/src/governance"
+
+    echo "🔬 folder: $(pwd) in under terraform: $ACTION action"
+    sh terraform.sh "$ACTION" devopslab &
+
+    pids+=($!)
+popd
 
 
 # Wait for each specific process to terminate.
