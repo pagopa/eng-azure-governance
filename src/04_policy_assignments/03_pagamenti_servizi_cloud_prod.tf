80ba77a8ce225e87dc5ce55cdde1142e19aeceee
pasqualedevita <>
2023-01-17 12:59:23 +0100

    fix: Exemption policy

--- src/04_policy_assignments/03_pagamenti_servizi_cloud_prod.tf
+++ src/04_policy_assignments/03_pagamenti_servizi_cloud_prod.tf
@@ -47,7 +47,7 @@ resource "azurerm_management_group_policy_exemption" "pagamenti_servizi_cloud_pr
 
 resource "azurerm_management_group_policy_exemption" "pagamenti_servizi_cloud_prod_azure_security_benchmark_waiver" {
   name                            = "${azurerm_management_group_policy_assignment.pagopa_azure_security_benchmark.name}${local.pagamenti_servizi_cloud_prod_prefix}-waiver"
-  management_group_id             = data.azurerm_management_group.pagamenti_servizi_cloud_dev.id
+  management_group_id             = data.azurerm_management_group.pagamenti_servizi_cloud_prod.id
   policy_assignment_id            = azurerm_management_group_policy_assignment.pagopa_azure_security_benchmark.id
   exemption_category              = "Waiver"
   description                     = "Motivation at https://pagopa.atlassian.net/wiki/spaces/DEVOPS/pages/608960613/Azure+Policy+-+Azure+Security+Benchmark"
--- src/04_policy_assignments/03_pagamenti_servizi_prod.tf
+++ src/04_policy_assignments/03_pagamenti_servizi_prod.tf
@@ -47,7 +47,7 @@ resource "azurerm_management_group_policy_exemption" "pagamenti_servizi_prod_iso
 
 resource "azurerm_management_group_policy_exemption" "pagamenti_servizi_prod_azure_security_benchmark_waiver" {
   name                            = "${azurerm_management_group_policy_assignment.pagopa_azure_security_benchmark.name}${local.pagamenti_servizi_prod_prefix}-waiver"
-  management_group_id             = data.azurerm_management_group.pagamenti_servizi_dev.id
+  management_group_id             = data.azurerm_management_group.pagamenti_servizi_prod.id
   policy_assignment_id            = azurerm_management_group_policy_assignment.pagopa_azure_security_benchmark.id
   exemption_category              = "Waiver"
   description                     = "Motivation at https://pagopa.atlassian.net/wiki/spaces/DEVOPS/pages/608960613/Azure+Policy+-+Azure+Security+Benchmark"
