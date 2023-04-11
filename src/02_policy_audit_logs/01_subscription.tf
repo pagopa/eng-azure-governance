fff3833c475cf911462decd56ddbaba4e5bdbfc4
pasqualedevita <>
2023-04-11 23:29:54 +0200

    fix audit log subscription

--- src/02_policy_audit_logs/01_subscription.tf
+++ src/02_policy_audit_logs/01_subscription.tf
@@ -1,7 +1,7 @@
 resource "azurerm_policy_definition" "audit_logs_subscription_log_analytics" {
   name                = "audit_logs_subscription_log_analytics"
   policy_type         = "Custom"
-  mode                = "Indexed"
+  mode                = "All"
   display_name        = "PagoPA Add Diagnostic Settings for audit logs for Subscription to Log Analytics workspace"
   management_group_id = data.azurerm_management_group.pagopa.id
 
@@ -16,9 +16,9 @@ resource "azurerm_policy_definition" "audit_logs_subscription_log_analytics" {
     }
 METADATA
 
-  parameters = file("./policy_rules/__subscription_log_analytics_parameters.json")
+  parameters = file("./policy_rules/subscription_log_analytics_parameters.json")
 
-  policy_rule = templatefile("./policy_rules/__subscription_log_analytics.json", {
+  policy_rule = templatefile("./policy_rules/subscription_log_analytics.json", {
     roleDefinitionIds_audit_logs_contributor    = data.azurerm_role_definition.audit_logs_contributor.id,
     roleDefinitionIds_log_analytics_contributor = data.azurerm_role_definition.log_analytics_contributor.id,
   })
@@ -28,7 +28,7 @@ METADATA
 resource "azurerm_policy_definition" "audit_logs_subscription_storage_account" {
   name                = "audit_logs_subscription_storage_account"
   policy_type         = "Custom"
-  mode                = "Indexed"
+  mode                = "All"
   display_name        = "PagoPA Add Diagnostic Settings for audit logs for Subscription to Storage Account"
   management_group_id = data.azurerm_management_group.pagopa.id
 
@@ -43,34 +43,7 @@ resource "azurerm_policy_definition" "audit_logs_subscription_storage_account" {
     }
 METADATA
 
-  parameters = <<PARAMETERS
-  {
-    "diagnosticsSettingName": {
-      "type": "String",
-      "metadata": {
-        "displayName": "Setting name",
-        "description": "Name of the diagnostic settings."
-      },
-      "defaultValue": "AuditLogs_StorageAccount"
-    },
-    "storageAccount": {
-      "type": "String",
-      "metadata": {
-        "displayName": "Storage Account",
-        "description": "Specify the Storage Account the Key Vault should be connected to.",
-        "assignPermissions": true
-      }
-    },
-    "location": {
-      "type": "String",
-      "metadata": {
-        "displayName": "Resource location",
-        "description": "Specify the resource location.",
-        "assignPermissions": true
-      }
-    }
-  }
-PARAMETERS
+  parameters = file("./policy_rules/subscription_storage_parameters.json")
 
   policy_rule = templatefile("./policy_rules/subscription_storage_account.json", {
     roleDefinitionIds_audit_logs_contributor    = data.azurerm_role_definition.audit_logs_contributor.id,
