937e2eb2332bafe7647d4f1d9516f853e05627bd
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-06-21 14:48:42 +0200

    pre-commit fixs

--- src/01_custom_roles/01_iac_reader.tf
+++ src/01_custom_roles/01_iac_reader.tf
@@ -5,13 +5,13 @@ resource "azurerm_role_definition" "iac_reader" {
 
   permissions {
     actions = [
-      "Microsoft.Resources/deployments/exportTemplate/action",                          # read arm template deployments
-      "Microsoft.Web/sites/config/list/action",                                         # read app config for function app, app service
-      "Microsoft.Web/sites/slots/config/list/action",                                   # read app config for function app, app service slots
-      "Microsoft.ContainerService/managedClusters/read",                                # help to generate cluster credentials
-      "Microsoft.ContainerService/managedClusters/listClusterUserCredential/action",      # help to generate cluster credentials
-      "Microsoft.ContainerService/managedClusters/accessProfiles/listCredential/action",  # help to generate cluster credentials and read cluster roles
-      "Microsoft.EventHub/namespaces/eventhubs/authorizationRules/listKeys/action",     #help to list key for event hub connection (mandatory for tf:azurerm_eventhub_authorization_rule)
+      "Microsoft.Resources/deployments/exportTemplate/action",                           # read arm template deployments
+      "Microsoft.Web/sites/config/list/action",                                          # read app config for function app, app service
+      "Microsoft.Web/sites/slots/config/list/action",                                    # read app config for function app, app service slots
+      "Microsoft.ContainerService/managedClusters/read",                                 # help to generate cluster credentials
+      "Microsoft.ContainerService/managedClusters/listClusterUserCredential/action",     # help to generate cluster credentials
+      "Microsoft.ContainerService/managedClusters/accessProfiles/listCredential/action", # help to generate cluster credentials and read cluster roles
+      "Microsoft.EventHub/namespaces/eventhubs/authorizationRules/listKeys/action",      #help to list key for event hub connection (mandatory for tf:azurerm_eventhub_authorization_rule)
     ]
   }
 }
