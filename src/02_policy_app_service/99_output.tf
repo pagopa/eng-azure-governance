02618c4b3b188a6afa53dd241cfa43a65e4bb9b2
fmlisco <{ID}+{username}@users.noreply.github.com>
2023-07-14 10:46:55 +0200

    add new sku policy

--- src/02_policy_app_service/99_output.tf
+++ src/02_policy_app_service/99_output.tf
@@ -2,6 +2,7 @@ output "policy_ids" {
   value = [
     azurerm_policy_definition.app_service_allowed_linuxfxversion.id,
     azurerm_policy_definition.function_app_allowed_linuxfxversion.id,
+    azurerm_policy_definition.app_service_allowed_sku.id,
   ]
 }
 
@@ -11,4 +12,8 @@ output "app_service_allowed_linuxfxversion_id" {
 
 output "function_app_allowed_linuxfxversion_id" {
   value = azurerm_policy_definition.function_app_allowed_linuxfxversion.id
+}
+
+output "app_service_allowed_sku_id" {
+  value = azurerm_policy_definition.app_service_allowed_sku.id
 }
