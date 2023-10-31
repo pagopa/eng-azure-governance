ad1065bc04a0a452f75bf3ca07cc7dd055c59761
pasqualedevita <>
2023-10-31 16:36:32 +0100

    fix

--- src/02_policy_container_apps/99_output.tf
+++ src/02_policy_container_apps/99_output.tf
@@ -4,6 +4,6 @@ output "policy_ids" {
   ]
 }
 
-output "container_apps_enforce_multiaz" {
+output "container_apps_enforce_multiaz_id" {
   value = azurerm_policy_definition.container_apps_enforce_multiaz.id
 }
--- src/03_policy_set/01_container_apps_prod.tf
+++ src/03_policy_set/01_container_apps_prod.tf
@@ -2,6 +2,7 @@ locals {
   container_apps_prod = {
     metadata_category_name = "pagopa_prod"
     enforce_multiaz = {
+      reference_id = "enforce_multiaz_reference_id"
       effect = "Audit"
     }
   }
@@ -18,12 +19,15 @@ resource "azurerm_policy_set_definition" "container_apps_prod" {
         "category": "${local.container_apps_prod.metadata_category_name}",
         "version": "v1.0.0",
         "ASC": "true",
-        "parameterScopes": {}
+        "parameterScopes": {
+          "${local.container_apps_prod.enforce_multiaz.reference_id} : ${local.container_apps_prod.enforce_multiaz.reference_id}": "${data.azurerm_management_group.pagopa.id}"
+        }
     }
 METADATA
 
   policy_definition_reference {
     policy_definition_id = data.terraform_remote_state.policy_container_apps.outputs.enforce_multiaz_id
+    reference_id         = local.container_apps_prod.enforce_multiaz.reference_id
     parameter_values = jsonencode({
       "effect" : {
         "value" : local.container_apps_prod.enforce_multiaz.effect
