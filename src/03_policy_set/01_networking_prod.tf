63aeaf8458c230d9486ca7ff1e1eb73b134ae833
Matteo Cellucci <matteo.cellucci@keypartner.com>
2023-11-03 14:54:23 +0100

    feat: TCE-142 Require AAD identities on VPN Gateway for auth (#107)
    Co-authored-by: pasqualedevita <>

--- src/03_policy_set/01_networking_prod.tf
+++ src/03_policy_set/01_networking_prod.tf
@@ -9,6 +9,10 @@ locals {
       reference_id = "ddos_protection_reference_id"
       plan_id      = "/subscriptions/0da48c97-355f-4050-a520-f11a18b8be90/resourceGroups/sec-p-ddos/providers/Microsoft.Network/ddosProtectionPlans/sec-p-ddos-protection"
     }
+    vpngw_aad_authentication = {
+      reference_id = "vpngw_aad_authentication_reference_id"
+      effect       = "Audit"
+    }
   }
 }
 
@@ -18,16 +22,15 @@ resource "azurerm_policy_set_definition" "networking_prod" {
   display_name        = "PagoPA Networking PROD"
   management_group_id = data.azurerm_management_group.pagopa.id
 
-  metadata = <<METADATA
-    {
-        "category": "${local.networking_prod.metadata_category_name}",
-        "version": "v1.0.0",
-        "ASC": "true",
-        "parameterScopes": {
-          "${local.networking_prod.ddos_protection.reference_id} : ${local.networking_prod.ddos_protection.reference_id}": "${data.azurerm_management_group.pagopa.id}"
-        }
+  metadata = jsonencode({
+    category = local.networking_prod.metadata_category_name
+    version  = "v1.0.0"
+    ASC      = "true"
+    parameterScopes = {
+      "${local.networking_prod.ddos_protection.reference_id} : ${local.networking_prod.ddos_protection.reference_id}"                   = data.azurerm_management_group.pagopa.id
+      "${local.networking_prod.vpngw_aad_authentication.reference_id} : ${local.networking_prod.vpngw_aad_authentication.reference_id}" = data.azurerm_management_group.pagopa.id
     }
-METADATA
+  })
 
   policy_definition_reference {
     policy_definition_id = data.terraform_remote_state.policy_networking.outputs.deny_zonal_publicip_id
@@ -38,13 +41,22 @@ METADATA
   policy_definition_reference {
     policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/94de2ad3-e0c1-4caf-ad78-5d47bbc83d3d"
     reference_id         = local.networking_prod.ddos_protection.reference_id
-    parameter_values     = <<VALUE
-    {
-      "ddosPlan": {
-        "value": ${jsonencode(local.networking_prod.ddos_protection.plan_id)}
+    parameter_values = jsonencode({
+      ddosPlan = {
+        value = local.networking_prod.ddos_protection.plan_id
       }
-    }
-    VALUE
+    })
+  }
+
+  # VPN gateways should use only Azure Active Directory (Azure AD) authentication for point-to-site users
+  policy_definition_reference {
+    policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/21a6bc25-125e-4d13-b82d-2e19b7208ab7"
+    reference_id         = local.networking_prod.vpngw_aad_authentication.reference_id
+    parameter_values = jsonencode({
+      effect = {
+        value = local.networking_prod.vpngw_aad_authentication.effect
+      }
+    })
   }
 }
 
