607426feb0742af3a36ef6884b35c3155858956b
pasqualedevita <>
2023-11-03 15:15:16 +0100

    Storage account force secure transfer enabled

--- src/03_policy_set/01_storage_account_prod.tf
+++ src/03_policy_set/01_storage_account_prod.tf
@@ -1,16 +1,22 @@
 locals {
   storage_account_prod = {
-    metadata_category_name                     = "pagopa_prod"
-    allowed_sku_policy_definition_reference_id = "allowed_sku"
-    allowed_sku = [
-      "Standard_ZRS",
-      "Standard_GRS",
-      "Standard_RAGRS",
-      "Standard_GZRS",
-      "Standard_RAGZRS",
-      "Premium_ZRS",
-    ]
-    allowed_sku_effect = "Audit"
+    metadata_category_name = "pagopa_prod"
+    allowed_sku = {
+      reference_id = "allowed_sku_reference_id"
+      effect       = "Audit"
+      skus = [
+        "Standard_ZRS",
+        "Standard_GRS",
+        "Standard_RAGRS",
+        "Standard_GZRS",
+        "Standard_RAGZRS",
+        "Premium_ZRS",
+      ]
+    }
+    secure_transfer = {
+      reference_id = "secure_transfer_reference_id"
+      effect       = "Deny"
+    }
   }
 }
 
@@ -20,16 +26,15 @@ resource "azurerm_policy_set_definition" "storage_account_prod" {
   display_name        = "PagoPA Storage Account PROD"
   management_group_id = data.azurerm_management_group.pagopa.id
 
-  metadata = <<METADATA
-    {
-        "category": "${local.storage_account_prod.metadata_category_name}",
-        "version": "v1.0.0",
-        "ASC": "true",
-        "parameterScopes": {
-          "${local.storage_account_prod.allowed_sku_policy_definition_reference_id} : ${local.storage_account_prod.allowed_sku_policy_definition_reference_id}": "${data.azurerm_management_group.pagopa.id}"
-        }
+  metadata = jsonencode({
+    category = local.storage_account_prod.metadata_category_name
+    version  = "v1.0.0"
+    ASC      = "true"
+    parameterScopes = {
+      "${local.storage_account_prod.allowed_sku.reference_id} : ${local.storage_account_prod.allowed_sku.reference_id}"         = data.azurerm_management_group.pagopa.id
+      "${local.storage_account_prod.secure_transfer.reference_id} : ${local.storage_account_prod.secure_transfer.reference_id}" = data.azurerm_management_group.pagopa.id
     }
-METADATA
+  })
 
   # Storage accounts should have the specified minimum TLS version (only TLS 1.2)
   policy_definition_reference {
@@ -40,7 +45,12 @@ METADATA
   # Secure transfer to storage accounts should be enabled
   policy_definition_reference {
     policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/404c3081-a854-4457-ae30-26a93ef643f9"
-    parameter_values     = jsonencode({})
+    reference_id         = local.storage_account_prod.secure_transfer.reference_id
+    parameter_values = jsonencode({
+      effect = {
+        value = local.storage_account_prod.secure_transfer.effect
+      }
+    })
   }
 
   # [Preview]: Storage account public access should be disallowed
@@ -52,17 +62,15 @@ METADATA
   # Storage accounts should be limited by allowed SKUs
   policy_definition_reference {
     policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/7433c107-6db4-4ad1-b57a-a76dce0154a1"
-    reference_id         = local.storage_account_prod.allowed_sku_policy_definition_reference_id
-    parameter_values     = <<VALUE
-    {
-      "listOfAllowedSKUs": {
-        "value": ${jsonencode(local.storage_account_prod.allowed_sku)}
-      },
-      "effect": {
-        "value": "${local.storage_account_prod.allowed_sku_effect}"
+    reference_id         = local.storage_account_prod.allowed_sku.reference_id
+    parameter_values = jsonencode({
+      listOfAllowedSKUs = {
+        value = local.storage_account_prod.allowed_sku.skus
       }
-    }
-    VALUE
+      effect = {
+        value = local.storage_account_prod.allowed_sku.effect
+      }
+    })
   }
 }
 
