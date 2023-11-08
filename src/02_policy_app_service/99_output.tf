9a1836f98adab9badceebe5bd7fda42b1a02f522
Matteo Cellucci <matteo.cellucci@keypartner.com>
2023-11-08 16:36:24 +0100

    chore: Refactor outputs

--- src/02_policy_app_service/99_output.tf
+++ src/02_policy_app_service/99_output.tf
@@ -1,9 +1,5 @@
 output "policy_ids" {
-  value = [
-    azurerm_policy_definition.app_service_allowed_linuxfxversion.id,
-    azurerm_policy_definition.function_app_allowed_linuxfxversion.id,
-    azurerm_policy_definition.app_service_allowed_sku.id,
-  ]
+  value = azurerm_policy_definition.*.id
 }
 
 output "app_service_allowed_linuxfxversion_id" {
