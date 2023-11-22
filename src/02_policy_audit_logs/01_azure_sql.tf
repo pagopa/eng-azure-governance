5aefea11885717a81eefccd77b8b4082b9491f18
pasqualedevita <>
2023-11-22 10:54:27 +0100

    feat: Add Azure SQL and Synapse Audit logs

---
+++ src/02_policy_audit_logs/01_azure_sql.tf
@@ -0,0 +1,92 @@
+resource "azurerm_policy_definition" "audit_logs_azure_sql_log_analytics" {
+  name                = "audit_logs_azure_sql_log_analytics"
+  policy_type         = "Custom"
+  mode                = "Indexed"
+  display_name        = "PagoPA Add Diagnostic Settings for audit logs for Azure SQL to Log Analytics workspace"
+  management_group_id = data.azurerm_management_group.pagopa.id
+
+  metadata = jsonencode({
+    category = var.metadata_category_name
+    version  = "v1.0.0"
+    securityCenter = {
+      RemediationDescription = "Add Diagnostic Settings for audit logs for Azure SQL to Log Analytics workspace"
+      Severity               = "High"
+    }
+  })
+
+  parameters = jsonencode({
+    diagnosticsSettingName = {
+      type = "String"
+      metadata = {
+        displayName = "Setting name"
+        description = "Name of the diagnostic settings."
+      }
+      defaultValue = "AuditLogs_LogAnalytics"
+    }
+    logAnalytics = {
+      type = "String"
+      metadata = {
+        displayName       = "Log Analytics workspace"
+        description       = "Specify the Log Analytics workspace the Key Vault should be connected to."
+        strongType        = "omsWorkspace"
+        assignPermissions = true
+      }
+    }
+  })
+
+  policy_rule = templatefile("./policy_rules/azure_sql_log_analytics.json", {
+    roleDefinitionIds_audit_logs_contributor    = data.azurerm_role_definition.audit_logs_contributor.id,
+    roleDefinitionIds_log_analytics_contributor = data.azurerm_role_definition.log_analytics_contributor.id,
+  })
+
+}
+
+resource "azurerm_policy_definition" "audit_logs_azure_sql_storage_account" {
+  name                = "audit_logs_azure_sql_storage_account"
+  policy_type         = "Custom"
+  mode                = "Indexed"
+  display_name        = "PagoPA Add Diagnostic Settings for audit logs for Azure SQL to Storage Account"
+  management_group_id = data.azurerm_management_group.pagopa.id
+
+  metadata = jsonencode({
+    category = var.metadata_category_name
+    version  = "v1.0.0"
+    securityCenter = {
+      RemediationDescription = "Add Diagnostic Settings for audit logs for Azure SQL to Storage Account"
+      Severity               = "High"
+    }
+  })
+
+  parameters = jsonencode({
+    diagnosticsSettingName = {
+      type = "String"
+      metadata = {
+        displayName = "Setting name"
+        description = "Name of the diagnostic settings."
+      }
+      defaultValue = "AuditLogs_StorageAccount"
+    }
+    storageAccount = {
+      type = "String"
+      metadata = {
+        displayName       = "Storage Account"
+        description       = "Specify the Storage Account the Key Vault should be connected to."
+        assignPermissions = true
+      }
+    }
+    location = {
+      type = "String"
+      metadata = {
+        displayName       = "Resource location"
+        description       = "Specify the resource location."
+        assignPermissions = true
+      }
+    }
+  })
+
+  policy_rule = templatefile("./policy_rules/azure_sql_storage_account.json", {
+    roleDefinitionIds_audit_logs_contributor    = data.azurerm_role_definition.audit_logs_contributor.id,
+    roleDefinitionIds_log_analytics_contributor = data.azurerm_role_definition.log_analytics_contributor.id,
+  })
+
+}
