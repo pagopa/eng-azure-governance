e2818ec201c0f1290e0c1584aaedf9ecf5181e0d
Matteo Cellucci <matteo.cellucci@keypartner.com>
2023-12-14 18:01:28 +0100

    feat: Use subscription policy assignment over group management for pci subscriptions (#128)
    Co-authored-by: pasqualedevita <>

---
+++ src/04_policy_assignments/03_dev_pci.tf
@@ -0,0 +1,27 @@
+locals {
+  dev_pci_subscriptions = [
+    "DEV-PCI",
+  ]
+}
+
+module "dev_pci_assignments" {
+  source = "./modules/dev_pci"
+
+  for_each = toset(local.dev_pci_subscriptions)
+
+  subscription   = [for s in data.azurerm_subscriptions.available.subscriptions : s if s.display_name == each.value][0]
+  location       = var.location
+  policy_set_ids = data.terraform_remote_state.policy_set.outputs
+
+  audit_logs = {
+    workspace_id                        = "/subscriptions/94004462-d636-48bc-aa63-ce22a99d6bf2/resourcegroups/pci-d-weu-core-monitor-rg/providers/microsoft.operationalinsights/workspaces/pci-d-weu-core-law"
+    storage_primary_region_storage_id   = "/subscriptions/ac17914c-79bf-48fa-831e-1359ef74c1d5/resourcegroups/devopslab-logs/providers/Microsoft.Storage/storageAccounts/devopslablogs"
+    storage_secondary_region_storage_id = "novalue"
+    storage_primary_region_location     = "westeurope"
+    storage_secondary_region_location   = "northeurope"
+  }
+
+  metrics_logs = {
+    workspace_id = "/subscriptions/94004462-d636-48bc-aa63-ce22a99d6bf2/resourcegroups/pci-d-weu-core-monitor-rg/providers/microsoft.operationalinsights/workspaces/pci-d-weu-core-law"
+  }
+}
---
+++ src/04_policy_assignments/modules/uat_pci/01_metrics_logs.tf
@@ -0,0 +1,35 @@
+resource "azurerm_subscription_policy_assignment" "metrics_logs" {
+  name                 = substr("${local.prefix}metricslogs", 0, 64)
+  display_name         = "PagoPA UAT PCI Metric logs"
+  policy_definition_id = var.policy_set_ids.metrics_logs_id
+  subscription_id      = var.subscription.id
+
+  location = var.location
+  enforce  = true
+  identity {
+    type = "SystemAssigned"
+  }
+
+  metadata = jsonencode({
+    category = var.metadata_category_name
+    version  = "v1.0.0"
+  })
+
+  parameters = jsonencode({
+    logAnalyticsId = {
+      value = var.metrics_logs.workspace_id
+    }
+  })
+}
+
+resource "azurerm_role_assignment" "metrics_logs_monitoring_contributor" {
+  scope                = var.subscription.id
+  role_definition_name = "PagoPA Audit Logs Contributor"
+  principal_id         = azurerm_subscription_policy_assignment.metrics_logs.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "metrics_logs_contributor_log_analytics" {
+  scope                = var.metrics_logs.workspace_id
+  role_definition_name = "Log Analytics Contributor"
+  principal_id         = azurerm_subscription_policy_assignment.metrics_logs.identity[0].principal_id
+}
