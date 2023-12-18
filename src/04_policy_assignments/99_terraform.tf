0c0c78879fecd16c4ec59bcbca5c24ce2c562add
pasqualedevita <>
2023-12-18 16:23:27 +0100

    remove policy

--- src/04_policy_assignments/99_terraform.tf
+++ src/04_policy_assignments/99_terraform.tf
@@ -11,6 +11,6 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfinforg"
     container_name       = "terraform-state"
-    key                  = "eng-azure-governance.policy_assignments.terraform.tfstate"
+    key                  = "eng-azure-governance.policy_assignments_old.terraform.tfstate"
   }
 }
