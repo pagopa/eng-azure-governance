fe353dca487b8f70694234682d9c1b1da8d1e18f
Matteo Cellucci <matteo.cellucci@keypartner.com>
2024-01-31 11:01:26 +0100

    chore: refactor audit logs pci policy definitions

--- src/03_policy_set/01_audit_logs.tf
+++ src/03_policy_set/01_audit_logs.tf
@@ -17,6 +17,28 @@ variable "audit_logs_storage_regions" {
   default = ["westeurope", "northeurope", "italynorth"]
 }
 
+locals {
+  audit_logs = {
+    reference_ids = {
+      api_management           = "api_management"
+      app_service              = "app_service"
+      application_gateway      = "application_gateway"
+      container_registry       = "container_registry"
+      cosmos_db                = "cosmos_db"
+      event_hub                = "event_hub"
+      grafana                  = "grafana"
+      keyvault                 = "keyvault"
+      kubernetes_cluster       = "kubernetes_cluster"
+      log_analytics            = "log_analytics_server"
+      postgresql_flexible      = "postgresql_flexible"
+      postgresql_single_server = "postgresql_single_server"
+      public_ip                = "public_ip"
+      subscription             = "subscription_westeurope"
+      virtual_network_gateway  = "virtual_network_gateway"
+    }
+  }
+}
+
 resource "azurerm_policy_set_definition" "audit_logs" {
   name                = "audit_logs"
   policy_type         = "Custom"
