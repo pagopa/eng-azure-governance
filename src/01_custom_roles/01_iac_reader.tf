161a2e90eb15e47f8d5972869fde0aa3d75602c4
Andrea Grillo <andrea.grillo@pagopa.it>
2024-06-07 11:50:15 +0200

    add action

--- src/01_custom_roles/01_iac_reader.tf
+++ src/01_custom_roles/01_iac_reader.tf
@@ -16,6 +16,7 @@ resource "azurerm_role_definition" "iac_reader" {
       "Microsoft.ServiceBus/namespaces/queues/authorizationRules/listKeys/action",
       "Microsoft.Cache/redis/listKeys/action",
       "Microsoft.Web/sites/host/listkeys/action",
+      "Microsoft.NotificationHubs/namespaces/notificationHubs/pnsCredentials/action", #list NotificationHub PNS credentials
     ]
   }
 }
