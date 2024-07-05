15439cd472a725923d583a925ed32b8be8321078
FilippoBau <filippo.baudanza@pagopa.it>
2024-07-05 11:09:38 +0200

    Added PagoPA DNS Takeover role

---
+++ src/01_custom_roles/01_dns_takeover.tf
@@ -0,0 +1,23 @@
+resource "azurerm_role_definition" "dns_takeover" {
+  name        = "PagoPA DNS Takeover"
+  scope       = data.azurerm_management_group.pagopa.id
+  description = "Read resources potentially vulnerable to subdomain takeover"
+
+  permissions {
+    actions = [
+      "Microsoft.Network/dnsZones/*/read",
+      "Microsoft.Network/frontDoors/read",
+      "Microsoft.Storage/storageAccounts/read",
+      "Microsoft.Cdn/profiles/endpoints/read",
+      "Microsoft.Cdn/profiles/afdendpoints/read",
+      "Microsoft.Network/internalPublicIpAddresses/read",
+      "Microsoft.Network/trafficManagerProfiles/read",
+      "Microsoft.ContainerInstance/containerGroups/read",
+      "Microsoft.ApiCenter/services/read",
+      "Microsoft.Web/sites/Read",
+      "Microsoft.Web/sites/slots/Read",
+      "Microsoft.ClassicCompute/domainNames/read",
+      "Microsoft.ClassicStorage/storageAccounts/read",
+    ]
+  }
+}
