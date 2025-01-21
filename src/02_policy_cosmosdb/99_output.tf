de5df7be21011a42da40e72dc9cf5ec97de3da23
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-01-21 15:11:36 +0100

    feat: Cosmos dynamic scaling policy

--- src/02_policy_cosmosdb/99_output.tf
+++ src/02_policy_cosmosdb/99_output.tf
@@ -6,6 +6,7 @@ output "policy_ids" {
     azurerm_policy_definition.cosmosdb_forbidden_capabilities.id,
     azurerm_policy_definition.cosmosdb_required_primary_zone_redundancy.id,
     azurerm_policy_definition.cosmosdb_forbidden_secondary_zone_redundancy.id,
+    azurerm_policy_definition.cosmos_dynamic_scaling_enabled.id
   ]
 }
 
@@ -31,4 +32,8 @@ output "cosmosdb_required_primary_zone_redundancy_id" {
 
 output "cosmosdb_forbidden_secondary_zone_redundancy_id" {
   value = azurerm_policy_definition.cosmosdb_forbidden_secondary_zone_redundancy.id
+}
+
+output "cosmos_dynamic_scaling_enabled_id" {
+  value = azurerm_policy_definition.cosmos_dynamic_scaling_enabled.id
 }
---
+++ src/02_policy_cosmosdb/dynamic_scaling_enabled.tf
@@ -0,0 +1,21 @@
+resource "azurerm_policy_definition" "cosmos_dynamic_scaling_enabled" {
+  name                = "cosmos_dynamic_scaling_enabled"
+  policy_type         = "Custom"
+  mode                = "Indexed"
+  display_name        = "PagoPA CosmosDB must have dynamic scaling enabled"
+  management_group_id = data.azurerm_management_group.pagopa.id
+
+  metadata = jsonencode({
+    category = var.metadata_category_name
+    version  = "v1.0.0"
+    securityCenter = {
+      RemediationDescription = "Enable Dynamic Scaling for CosmosDB"
+      Severity               = "High"
+    }
+  })
+
+  parameters = file("./policy_rules/dynamic_scaling_enabled_parameters.json")
+
+  policy_rule = file("./policy_rules/dynamic_scaling_enabled_policy.json")
+
+}
