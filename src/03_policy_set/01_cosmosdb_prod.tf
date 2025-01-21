72c57715c8bf9a84d0223001c928b052ae59e161
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-01-21 15:57:34 +0100

    feat: Cosmos dynamic scaling policy (#161)
    Co-authored-by: Pasquale De Vita <pasquale.devita@pagopa.it>

--- src/03_policy_set/01_cosmosdb_prod.tf
+++ src/03_policy_set/01_cosmosdb_prod.tf
@@ -39,6 +39,12 @@ resource "azurerm_policy_set_definition" "cosmosdb_prod" {
     policy_definition_id = data.terraform_remote_state.policy_cosmosdb.outputs.cosmosdb_forbidden_secondary_zone_redundancy_id
     parameter_values     = jsonencode({})
   }
+
+  policy_definition_reference {
+    policy_definition_id = data.terraform_remote_state.policy_cosmosdb.outputs.cosmosdb_dynamic_scaling_enabled_id
+    parameter_values     = jsonencode({})
+  }
+
 }
 
 output "cosmosdb_prod_id" {
