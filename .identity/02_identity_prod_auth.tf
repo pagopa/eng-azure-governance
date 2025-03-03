9faec7135a3d71990082698caffa44968d5f8b9d
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-03-03 17:40:54 +0100

    feat: Script and workflow for policy remediation (#168)
    Co-authored-by: Pasquale De Vita <pasquale.devita@pagopa.it>
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- .identity/02_identity_prod_auth.tf
+++ .identity/02_identity_prod_auth.tf
@@ -4,6 +4,12 @@ resource "azurerm_role_assignment" "prod_policy_reader" {
   principal_id         = azurerm_user_assigned_identity.prod.principal_id
 }
 
+resource "azurerm_role_assignment" "prod_policy_remediator" {
+  scope                = data.azurerm_management_group.pagopa.id
+  role_definition_name = "PagoPA Policy Remediator"
+  principal_id         = azurerm_user_assigned_identity.prod.principal_id
+}
+
 resource "azurerm_role_assignment" "prod_management_group_reader" {
   scope                = data.azurerm_management_group.pagopa.id
   role_definition_name = "Management Group Reader"
---
+++ src/scripts/policy_remediation.sh
@@ -0,0 +1,40 @@
+az extension show --name account || az extension add --name account
+subscriptions=$(az account subscription list --query "[].subscriptionId" -o tsv)
+policySetIds=(
+    '/providers/Microsoft.Management/managementGroups/pagopa/providers/Microsoft.Authorization/policySetDefinitions/audit_logs'
+    '/providers/Microsoft.Management/managementGroups/pagopa/providers/Microsoft.Authorization/policySetDefinitions/resource_lock'
+)
+
+for subscriptionId in $subscriptions; do
+    az account set --subscription $subscriptionId
+
+    # Loop through each policy set ID
+    for policySetId in "${policySetIds[@]}"; do
+
+        # Get the list of policy assignments and filter by the policy set
+        policyAssignments=$(az policy assignment list --query "[?policyDefinitionId=='$policySetId'].name" -o tsv)
+
+        # Loop through each policy assignment and check compliance state
+        for policyAssignmentName in $policyAssignments; do
+            echo "Processing policy assignment: $policyAssignmentName"
+
+            # Get the compliance state of resources for this policy assignment
+            complianceStates=$(az policy state list --policy-assignment $policyAssignmentName --query "[?complianceState=='NonCompliant'].policyDefinitionReferenceId" -o tsv | sort | uniq)
+
+            # Check if there are any non-compliant resources
+            if [[ -z "$complianceStates" ]]; then
+                echo "No resources found for policy assignment: $policyAssignmentName"
+            else
+                for policyDefinitionReferenceId in $complianceStates; do
+                    echo "Remediating resource for policy definition: $policyDefinitionReferenceId"
+                    # Command to remediate the resource
+                    timestamp=$(date +%s)
+                    # echo "DRY RUN: az policy remediation create --name "remediationTask$timestamp" --policy-assignment $policyAssignmentName --definition-reference-id $policyDefinitionReferenceId"
+                    az policy remediation create --name "remediationTask$timestamp" --policy-assignment $policyAssignmentName --definition-reference-id $policyDefinitionReferenceId
+                done
+            fi
+        done
+    done
+done
+
+
