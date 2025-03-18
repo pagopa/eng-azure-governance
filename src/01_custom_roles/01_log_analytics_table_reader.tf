5c9efe25d5d3f9eb915d811d7cec7f0dddbdb8da
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-03-18 12:41:18 +0100

    feat: Log Analytics table reader role

---
+++ src/01_custom_roles/01_log_analytics_table_reader.tf
@@ -0,0 +1,14 @@
+resource "azurerm_role_definition" "log_analytics_table_reader" {
+  name        = "PagoPA Log Analytics Table Reader"
+  scope       = data.azurerm_management_group.pagopa.id
+  description = "Read from Log Analytics Table"
+  permissions {
+    actions = [
+      "Microsoft.OperationalInsights/workspaces/read",
+      "Microsoft.OperationalInsights/workspaces/query/read",
+    ]
+    not_actions = [
+    "Microsoft.OperationalInsights/workspaces/sharedKeys/read"
+    ]
+  }
+}
