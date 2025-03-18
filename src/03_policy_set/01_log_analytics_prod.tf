70b69c1a127b2009e3600c7dce73c365e5d2943b
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-03-18 10:37:56 +0100

    feat: Log Analytics allowed sku policy

--- src/03_policy_set/01_log_analytics_prod.tf
+++ src/03_policy_set/01_log_analytics_prod.tf
@@ -22,7 +22,7 @@ resource "azurerm_policy_set_definition" "log_analytics_prod" {
   }
 
   policy_definition_reference {
-    policy_definition_id = data.terraform_remote_state.policy_log_analytics.outputs.log_analytics_link_dedicated_cluster_id
+    policy_definition_id = data.terraform_remote_state.policy_log_analytics.outputs.log_analytics_allowed_sku_id
     parameter_values     = jsonencode({})
   }
 }
