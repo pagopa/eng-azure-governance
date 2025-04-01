11bc8e65d74831854143fbc7ebbea7f9f7a7c52c
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-04-01 17:28:08 +0200

    feat: removed forbidden capabilities policy

--- src/02_policy_cosmosdb/99_output.tf
+++ src/02_policy_cosmosdb/99_output.tf
@@ -3,10 +3,11 @@ output "policy_ids" {
     azurerm_policy_definition.cosmosdb_required_network.id,
     azurerm_policy_definition.cosmosdb_allowed_tls.id,
     azurerm_policy_definition.cosmosdb_required_backup_policy.id,
-    azurerm_policy_definition.cosmosdb_forbidden_capabilities.id,
     azurerm_policy_definition.cosmosdb_required_primary_zone_redundancy.id,
     azurerm_policy_definition.cosmosdb_forbidden_secondary_zone_redundancy.id,
-    azurerm_policy_definition.cosmosdb_dynamic_scaling_enabled.id
+    azurerm_policy_definition.cosmosdb_dynamic_scaling_enabled.id,
+    azurerm_policy_definition.cosmosdb_automatic_failover_enabled.id,
+    azurerm_policy_definition.cosmosdb_allowed_capacity_mode.id
   ]
 }
 
@@ -20,10 +21,6 @@ output "cosmosdb_allowed_tls_id" {
 
 output "cosmosdb_required_backup_policy_id" {
   value = azurerm_policy_definition.cosmosdb_required_backup_policy.id
-}
-
-output "cosmosdb_forbidden_capabilities_id" {
-  value = azurerm_policy_definition.cosmosdb_forbidden_capabilities.id
 }
 
 output "cosmosdb_required_primary_zone_redundancy_id" {
