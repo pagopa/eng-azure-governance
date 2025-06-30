a5be1809ef40891500c3eb8631432454a1eae9d0
maurodandrea <ing.mauro.dandrea@gmail.com>
2025-06-30 18:36:11 +0200

    fmt

--- src/02_policy_app_service/01_app_service_apps_allowed_tlsversion.tf
+++ src/02_policy_app_service/01_app_service_apps_allowed_tlsversion.tf
@@ -1,10 +1,10 @@
 # Add Custom Policy for tls 1.2 and 1.3
 resource "azurerm_policy_definition" "app_service_apps_tls_custom" {
-  name         = "app-service-apps-allow-tls-1-2-and-1-3"
-  policy_type  = "Custom"
-  mode         = "Indexed"
-  display_name = "App Service Apps should use TLS 1.2 or 1.3"
-  description  = "Ensures App Service Apps use TLS 1.2 or 1.3"
+  name                = "app-service-apps-allow-tls-1-2-and-1-3"
+  policy_type         = "Custom"
+  mode                = "Indexed"
+  display_name        = "App Service Apps should use TLS 1.2 or 1.3"
+  description         = "Ensures App Service Apps use TLS 1.2 or 1.3"
   management_group_id = data.azurerm_management_group.pagopa.id
 
   metadata = jsonencode({
@@ -16,7 +16,7 @@ resource "azurerm_policy_definition" "app_service_apps_tls_custom" {
     }
   })
 
-  parameters = file("./policy_rules/allowed_app_service_apps_tlsversion_parameters.json")
+  parameters  = file("./policy_rules/allowed_app_service_apps_tlsversion_parameters.json")
   policy_rule = file("./policy_rules/allowed_app_service_apps_tlsversion_policy.json")
 
 }
