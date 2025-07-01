f08c9dbeacb9a88b43d918d8c2daed2d540b1a60
maurodandrea <ing.mauro.dandrea@gmail.com>
2025-07-01 10:57:55 +0200

    add-uat-prod

--- src/03_policy_set/01_app_service_uat.tf
+++ src/03_policy_set/01_app_service_uat.tf
@@ -56,13 +56,13 @@ resource "azurerm_policy_set_definition" "app_service_uat" {
 
   # Function apps should use the latest TLS version
   policy_definition_reference {
-    policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/f9d614c5-c173-4d56-95a7-b4437057d193"
+    policy_definition_id = data.terraform_remote_state.policy_app_service.outputs.function_app_allowed_tls_id
     parameter_values     = jsonencode({})
   }
 
   # App Service apps should use the latest TLS version
   policy_definition_reference {
-    policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/f0e6e85b-9b9f-4a4b-b67b-f730d42f1b0b"
+    policy_definition_id = data.terraform_remote_state.policy_app_service.outputs.app_service_allowed_tls_id
     parameter_values     = jsonencode({})
   }
 
