ff7061ede33c48aea6236758062c24c8ce47bf56
Mario Mupo <mario.mupo@pagopa.it>
2025-09-23 09:24:33 +0200

    fix: minor

--- src/01_custom_roles/01_static_web_app_list_secrets.tf
+++ src/01_custom_roles/01_static_web_app_list_secrets.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_definition" "static_web_app_secrets" {
-  name        = "PagoPA Static Web Apps list secrets"
+  name        = "PagoPA Static Web Apps List Secrets"
   scope       = data.azurerm_management_group.pagopa.id
   description = "List-only access to Static Web App secrets"
 
